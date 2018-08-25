37207f86711000ef58b18c56f7c0a1081ccf2222
Dhanashree <dvphalak@gmail.com>
2018-08-25 18:28:47 -0400

    All done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,44 +5,69 @@
 // 5. if score equals random number user wins
 // 6. if score greater than number user looses
 // 7. in both conditions computer will pick new number for next round.
+
 $(document).ready(function () {
-  // selects random number between 1-100
-  var randomNumber = (Math.floor(Math.random() * 120)) + 1;
-  $("#random").html(randomNumber);
-
-  $("img").addClass("image_clk");
-
-  //  selects 
-  var ytotal = 0;
+  var wins = 0;
+  var losses = 0;
+  var gameIsRunning;
+  var ytotal;
+  var randomNumber;
   var numberOptions = [1, 8, 4, 2];
 
-  $(".pink").attr("data_crystalValue", numberOptions[0]);
-  $(".yellow").attr("data_crystalValue", numberOptions[1]);
-  $(".green").attr("data_crystalValue", numberOptions[2]);
-  $(".blue").attr("data_crystalValue", numberOptions[3]);
+  function newGame() {
+    // selects random number between 1-100
+    randomNumber = (Math.floor(Math.random() * 120)) + 1;
+    $("#random").html(randomNumber);
+    $(".none").hide();
+    gameIsRunning = true;
+    $("img").addClass("image_clk");
 
-  $(".image_clk").on("click", function () {
-    var crystalValue = $(this).attr("data_crystalValue");
-    crystalValue = parseInt(crystalValue);
-    ytotal += crystalValue;
+    //  selects 
+    ytotal = 0;
+
+    $(".pink").attr("data_crystalValue", numberOptions[0]);
+    $(".yellow").attr("data_crystalValue", numberOptions[1]);
+    $(".green").attr("data_crystalValue", numberOptions[2]);
+    $(".blue").attr("data_crystalValue", numberOptions[3]);
+    $(".instructions").show();
+    $('#win').hide();
+    $('#loose').hide();
     $("#total").html(ytotal);
-    console.log("total " + ytotal);
-    console.log("crystal val " + crystalValue);
 
-    if (ytotal == randomNumber) {
-      console.log("you win");
-      $(".instructions").html("You Won!!!");
-      $(".first").css({'display':'none'});
-      $(".none").css({'display':'inherit'});
-      // $(".winning_screen").css({'background-imge':'url("https://giphy.com/stickers/cute-kawaii-1gP0UvJKH9Hqc4EJAa")'});
+  }
+  newGame()
+  $(".image_clk").on("click", function () {
+    if (gameIsRunning) {
+      var crystalValue = $(this).attr("data_crystalValue");
+      crystalValue = parseInt(crystalValue);
+      ytotal += crystalValue;
+      $("#total").html(ytotal);
+    }
+    if ((ytotal == randomNumber)) {
+      wins++;
+      $(".wins").html(wins);
+      $(".instructions").hide();
+      $('#win').show();
+      $(".first").css({ 'display': 'none' });
+      $(".none").css({ 'display': 'inherit' });
+      gameIsRunning = false;
+      $(".newNum").on("click", function () {
+        newGame()
+      })
+
     }
     else if (ytotal > randomNumber) {
-      $(".instructions").html("You loose...Try Again");
+      losses++;
+      $(".losses").html(losses);
+      $(".instructions").hide();
+      $('#loose').show();
+      gameIsRunning = false;
+      $(".newNum").on("click", function () {
+        newGame()
+      })
     }
   })
 
-  // how can i make each number selection unique?
 
 })
-
 
